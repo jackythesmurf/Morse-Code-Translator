6f9586c40e888e931fec257392e88ae1ffc60a9b
jackythesmurf <Jackythesmurf@gmail.com>
2023-01-15 14:19:50 +1100

    reverse commit, due to bug

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,58 @@
 import { morseCodeDictionary } from "./dictionary.js";
-import {playMorseLive} from "./sound.js"
+
 // Initial Start Up Code
 let pressOnce = true;
+
+const playMorseLive = (letter, live) => {
+  var AudioContext = window.AudioContext;
+  var ctx = new AudioContext();
+  var dot = 1.2 / 15;
+  var t = ctx.currentTime;
+
+  var oscillator = ctx.createOscillator();
+  oscillator.type = "sine";
+  oscillator.frequency.value = 600;
+
+  var gainNode = ctx.createGain();
+  gainNode.gain.setValueAtTime(0, t);
+
+  const setValuePerChar = (char) => {
+    switch (char) {
+      case ".":
+        gainNode.gain.setValueAtTime(1, t);
+        t += dot;
+        gainNode.gain.setValueAtTime(0, t);
+        t += dot;
+        break;
+      case "-":
+        gainNode.gain.setValueAtTime(1, t);
+        t += 3 * dot;
+        gainNode.gain.setValueAtTime(0, t);
+        t += dot;
+        break;
+      case " ":
+        t += 7 * dot;
+        break;
+    }
+  };
+  if (live == true) {
+    setValuePerChar(letter);
+  } else {
+    letter.split("").forEach((char) => {
+      setValuePerChar(char);
+    });
+    let i = 0
+    setInterval(() => {
+      i++
+      if( i == letter.length) {
+        pressOnce = true
+      }
+    }, 200);
+  }
+  oscillator.connect(gainNode);
+  gainNode.connect(ctx.destination);
+  oscillator.start();
+};
 
 const translate = (englishInput) => {
   let translation = "";
