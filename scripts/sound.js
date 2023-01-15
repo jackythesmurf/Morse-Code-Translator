c349ce2e07a35ed445155ef3fabd7ac5171f1974
jackythesmurf <Jackythesmurf@gmail.com>
2023-01-15 13:01:46 +1100

    added seperate modules for sound

---
+++ scripts/sound.js
@@ -0,0 +1,50 @@
+export const playMorseLive = (letter, live) => {
+    var AudioContext = window.AudioContext;
+    var ctx = new AudioContext();
+    var dot = 1.2 / 15;
+    var t = ctx.currentTime;
+  
+    var oscillator = ctx.createOscillator();
+    oscillator.type = "sine";
+    oscillator.frequency.value = 600;
+  
+    var gainNode = ctx.createGain();
+    gainNode.gain.setValueAtTime(0, t);
+  
+    const setValuePerChar = (char) => {
+      switch (char) {
+        case ".":
+          gainNode.gain.setValueAtTime(1, t);
+          t += dot;
+          gainNode.gain.setValueAtTime(0, t);
+          t += dot;
+          break;
+        case "-":
+          gainNode.gain.setValueAtTime(1, t);
+          t += 3 * dot;
+          gainNode.gain.setValueAtTime(0, t);
+          t += dot;
+          break;
+        case " ":
+          t += 7 * dot;
+          break;
+      }
+    };
+    if (live == true) {
+      setValuePerChar(letter);
+    } else {
+      letter.split("").forEach((char) => {
+        setValuePerChar(char);
+      });
+      let i = 0
+      setInterval(() => {
+        i++
+        if( i == letter.length) {
+          pressOnce = true
+        }
+      }, 200);
+    }
+    oscillator.connect(gainNode);
+    gainNode.connect(ctx.destination);
+    oscillator.start();
+  };
